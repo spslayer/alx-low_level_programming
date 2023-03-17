88d695fade6aa2b417a94112bc3f235831de662f
spslayer <aaichoauabdellah@gmail.com>
2023-03-16 17:10:32 -0700

    hello univers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 /**
- * this is a programm that uses printf
+ * main - Prints the size of various typesa based on
+ * the coputer it is compiled and run on..
+ * Return: Always 0.
  */
+
 int main(void)
+
 {
 	/*this is main*/
 	char a;
@@ -11,10 +15,10 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
