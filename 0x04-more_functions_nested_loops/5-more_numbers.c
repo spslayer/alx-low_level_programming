3b51d654dbf2f1eef00c2eab093f6425b9b98c9d
spslayer <aaichoauabdellah@gmail.com>
2023-03-23 15:06:09 -0700

    c lang

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,14 @@ void more_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y <= 14; y++)
-		{
-			if (y > 0)
-			{
-				_putchar((y / 10) + '0');
-			}
-			_putchar((y % 10) + '0');
-		}
-		_putchar('\n');
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 9)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
+	}
+	_putchar('\n');
 	}
 }
